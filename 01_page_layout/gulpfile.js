328eb5cda6d28bc1e47c5e64c1364da4b8386ea5
Hindo <piotr.pabich@gmail.com>
2017-11-26 23:24:29 +0100

    [01_page_layout] Add less autopreffixer

--- 01_page_layout/gulpfile.js
+++ 01_page_layout/gulpfile.js
@@ -1,10 +1,13 @@
 const gulp = require('gulp')
 const less = require('gulp-less')
 const path = require('path')
+const LessPluginAutoPrefix = require('less-plugin-autoprefix')
+const autoprefixPlugin = new LessPluginAutoPrefix({browsers: ["last 2 versions"]})
 
 gulp.task('less', () => {
   const le = less({
-    paths: [path.join(__dirname, 'less', 'includes')]
+    paths: [path.join(__dirname, 'less', 'includes')],
+    plugins: [autoprefixPlugin]
   }).on('error', (e) => {
     console.log(e.message)
     le.end()
